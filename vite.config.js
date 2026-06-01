2f6761d21098651595f4dc5d1e2b445c1b5aa3de
Vincent-12156 <12156@holbertonstudents.com>
2026-06-01 09:00:36 +0200

    Fix vite coonfig

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/lune-e/",
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
 });
